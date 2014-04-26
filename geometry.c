302900c1008114a801c11d561334e28a93f6f335
Vignesh Prabhu <me@viggy.in>
2014-04-26 11:43:04 +0530

    Added Stub for geometry application

---
+++ geometry.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	// stub for intro message and switch case statements
+	return 0;
+}
+
+int calAreaOfSquare()
+{
+    // stub for calculating Area of Square
+}
+int calAreaOfTriangle()
+{
+    // stub for calculating Area of Triangle 
+}
+int calAreaOfRectangle()
+{
+    // stub for calculating Area of Rectangle
+}
+int calAreaOfCircle()
+{
+    // stub for calculating Area of Circle
+}
