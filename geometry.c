27d848dd6e8743a7d7fbfcb5103e41ef5257a8ea
vinit <vinit@vin>
2014-04-26 15:22:20 +0530

    hello something is done

--- geometry.c
+++ geometry.c
@@ -3,22 +3,27 @@
 int main()
 {
 	// stub for intro message and switch case statements
+	
 	return 0;
 }
 
-int calAreaOfSquare()
+int calAreaOfSquare(int side)
 {
     // stub for calculating Area of Square
+	return side*side;
 }
-int calAreaOfTriangle()
+int calAreaOfTriangle(int h,int b)
 {
     // stub for calculating Area of Triangle 
+	return (1/2.0)*b*h;
 }
-int calAreaOfRectangle()
+int calAreaOfRectangle(int a,int b)
 {
     // stub for calculating Area of Rectangle
+	return a*b;
 }
-int calAreaOfCircle()
+int calAreaOfCircle(int r)
 {
     // stub for calculating Area of Circle
+	return 3.14*r*r;
 }
